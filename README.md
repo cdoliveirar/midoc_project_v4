c9a7b3000dae0661d603fb94bf971028506c03da
Carlos Oliveira <cdoliveirar@gmail.com>
2017-11-09 10:54:30 -0500

    first commit midoc_v4

---
+++ README.md
@@ -0,0 +1,4 @@
+MiDoc Project is a backend application
+for Rest implementation
+
+rest api implementation
---
+++ midoc/api/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+from .models import (Patient,
+                     Doctor,
+                     Enterprise,
+                     MedicalHistory,
+                     ArtifactMeasurement,
+                     )
+
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = '__all__'
+
+
+class EnterpriseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enterprise
+        fields = '__all__'
+
+
+class DoctorLocalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = ('id', 'doctor_name', 'picture_url')
+
+
+class MedicalHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MedicalHistory
+        fields = '__all__'
+        #depth = 1
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = '__all__'
+
+
+# without model
+class PatientVerifySerializer(serializers.Serializer):
+    dni = serializers.CharField(max_length=8)
+    enterprise_id = serializers.IntegerField()
+
+
+# artifact
+class ArtifactMeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArtifactMeasurement
+        fields = '__all__'
+
+
